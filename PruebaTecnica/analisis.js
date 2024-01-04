8397183e37357f63db47e8eba62f8af1aa724817
zerialfan <zerialfan@gmail.com>
2024-01-04 10:14:46 -0600

    Add function using reduce and slice method

--- PruebaTecnica/analisis.js
+++ PruebaTecnica/analisis.js
@@ -19,5 +19,43 @@ function salarioPromedio(nombrePersona) {
     return promedio
 }
 
-// Proyección de salarios 
+// Proyección de salario personal basandose en el ultimo año
+// PS= ultimo salario X crecimiento Salarial promedio 
+function crecimientoSalarial(nombrePersona) {
+// la variable obtiene el array de la fx obtener salario (vacio o undefined)
+    const tasaDeCrecimiento = obtenerSalario(nombrePersona)
+
+// Se crea un nuevo array para ejecutar la logica dentro de este
+    const arrSalarios =[]
+// recorre el array iniciando en la segunda posicion y lo resta con el anterior luego lo divide entre el primer indice.
+    for (let i = 1; i < tasaDeCrecimiento.length; i++) {
+        const crecimiento =(tasaDeCrecimiento[i]-tasaDeCrecimiento[i-1])/tasaDeCrecimiento[i-1]
+// El resultado se empuja al array 'arrSalarios' y usa como parametro crecimiento que es donde esta la operacion logica
+        arrSalarios.push(crecimiento)
+    }
+// Luego se obtiene el promedio de los datos
+    const promedioSalarial = PlatziMath.calcularPromedio(arrSalarios)
+// El ultimo salario para hacer el calculo a partir de este 
+    const ultimoSalario = tasaDeCrecimiento[tasaDeCrecimiento.length-1]
+// esta variable es la logica de 'PS' o proyeccion de salario, parseInt para que no agregue decimales
+    const nuevoSalario = parseInt(ultimoSalario * (1+promedioSalarial))
+// Se regresa este resultado como unico para fines practicos
+    return nuevoSalario
+}
+// Esta funcion es la misma que crecimiento salarial pero usando el metodo slice y reduce
+function crecimientoReduce(nombrePersona) {
+    const money= obtenerSalario(nombrePersona)
+
+    const arrSalarios= money.slice(1).reduce((resultado, salario,index)=>{
+        const crecimiento = (salario -money[index])/money[index]
+        resultado.push(crecimiento)
+        return resultado
+    },[])
+    const promedioSalarial = PlatziMath.calcularPromedio(arrSalarios)
+
+    const ultimoSalario = money[money.length - 1]
+    const nuevoSalario = parseInt(ultimoSalario*(1+promedioSalarial))
+
+    return nuevoSalario
+}
 
